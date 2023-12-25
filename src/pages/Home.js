25a22f9a2fec4e798798798a400a92fa4c53c9d2
David Nguyen <creativespecials@gmail.com>
2023-12-25 17:52:56 +1030

    Updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -58,40 +58,6 @@ export default function Home() {
         look forward to a future filled with innovative ideas, collaborative
         projects, and a lasting impact on the world.
       </p>
-      <div>
-        <h2>Photos of Cats because I like Cats:</h2>
-        <img
-          src="https://img.freepik.com/free-photo/red-white-cat-i-white-studio_155003-13189.jpg?w=2000"
-          alt="Example"
-          style={{ maxWidth: "100%", maxHeight: "400px", height: "auto" }}
-        />
-        <img
-          src="https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg"
-          alt="Example"
-          style={{ maxWidth: "100%", maxHeight: "400px", height: "auto" }}
-        />
-        <img
-          src="https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80"
-          alt="Example"
-          style={{ maxWidth: "100%", maxHeight: "400px", height: "auto" }}
-        />
-        <img
-          src="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.88847xw:1xh;center,top&resize=1200:*"
-          alt="Example"
-          style={{ maxWidth: "100%", maxHeight: "400px", height: "auto" }}
-        />
-        <img
-          src="https://images.theconversation.com/files/443350/original/file-20220131-15-1ndq1m6.jpg?ixlib=rb-1.1.0&rect=0%2C0%2C3354%2C2464&q=45&auto=format&w=926&fit=clip"
-          alt="Example"
-          style={{ maxWidth: "100%", maxHeight: "400px", height: "auto" }}
-        />
-        <img
-          src="https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&w=1000&q=80"
-          alt="Example"
-          style={{ maxWidth: "100%", maxHeight: "400px", height: "auto" }}
-        />
-        {/* Add more images here if you want to create an image gallery */}
-      </div>
     </div>
   );
 }
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,23 +2,62 @@ import React from 'react';
 
 const Resume = () => {
   const skills = [
-    'HTML5', 'CSS', 'JavaScript', 'Express', 'jQuery', 'Jest',
+    'HTML', 'CSS', 'JavaScript', 'Express', 'jQuery', 'Jest',
     'Markdown', 'Node.js', 'Bootstrap', 'NPM', 'Sequelize', 'MySQL',
     'Git', 'Heroku', 'Insomnia', 'Visual Studio Code', 'GitHub',
-    'JSON', 'MongoDB', 'Handlebars', 'Tailwind CSS', 'Webpack', 'GitLab'
+    'JSON', 'MongoDB', 'Handlebars', 'Tailwind CSS', 'Webpack', 'GitLab',
+    'React', 'GraphQL', 'Mongoose'
   ];
 
   const workExperience = [
-    '3 Years of Carpentry with Western Technical College',
-    'Few months of Work Experience with Paint Machine',
-    'Few months of Work Experience with AMA Group (All transport Crash Repairs)'
+    'University of Adelaide - Full Stack Web Developer Student (March 2023 – September 2023)',
+    'Western Technical College - Carpentry (3 Years)',
+    'Paint Machine - Work Experience (Few months)',
+    'AMA Group (All transport Crash Repairs) - Work Experience (Few months)'
   ];
 
   const education = [
+    'University of Adelaide - Certificate of Completion in Web Development (Graduation: 2023)',
     'Mount Carmel College: Completion of Year 12',
     'Vet Courses (WTC): Introduction to trades, Cert I, II & III (Carpentry)',
     'Tool Usages, EWP certificate, safety training & White Card',
     'Bootcamp from "The University of Adelaide": Coding/Web Programmer'
+  ];
+
+  const additionalSkills = [
+    'Google Data Analytics',
+    'SEO (On- and Off-Page)',
+    'Communication',
+    'Attention to Detail',
+    'Team Player'
+  ];
+
+  const certifications = [
+    {
+      name: 'Entrepreneurship & Innovation: Web Development (Forage)',
+      date: 'December 2023',
+      details: [
+        'Developed expertise in planning and structuring websites.',
+        'Translated design concepts into responsive web elements.',
+        'Applied entrepreneurial thinking to web development.'
+      ]
+    },
+    {
+      name: 'Google Data Analytics 4',
+      date: 'December 2023',
+      details: [
+        'Developed data-driven decisions involving analyzing various aspects of the site.',
+        'Explored diverse reports covering acquisition, engagement, monetization, retention, user attributes, and tech.'
+      ]
+    },
+    {
+      name: 'SEO Certificate (Great Learning)',
+      date: 'December 2023',
+      details: [
+        'Learned about automated marketing, content marketing strategies, search engine advertising, keywords, PPC campaigns, and public relations.',
+        'Learned about on-page SEO and off-page SEO strategies.'
+      ]
+    }
   ];
 
   return (
@@ -27,11 +66,7 @@ const Resume = () => {
 
       <h2>Summary</h2>
       <p>
-        Highly positive and adaptable individual with a strong work ethic. I approach
-        challenging tasks with enthusiasm and a willingness to learn. Safety is a top
-        priority, and I excel in collaborative environments, communicating effectively
-        with colleagues to resolve issues. I thrive on hands-on experiences and enjoy
-        exploring new activities to expand my knowledge and skills.
+        I am a highly positive and adaptable individual with a strong work ethic. I approach challenging tasks with enthusiasm and a willingness to learn. Safety is a top priority, and I excel in collaborative environments, communicating effectively with colleagues to resolve issues. I thrive on hands-on experiences and enjoy exploring new activities to expand my knowledge and skills.
       </p>
 
       <h2>Technical Skills</h2>
@@ -43,19 +78,23 @@ const Resume = () => {
 
       <h2>Skills</h2>
       <ul>
-        <li>Proficient in handling basic PPE/safety protocols</li>
-        <li>Experienced in EWP (Elevated Work Platform) operation with BL and SL</li>
-        <li>Skilled in painting and carpentry, with a focus on automotive spray painting</li>
-        <li>Quick learner and adapt easily to new challenges</li>
+        {additionalSkills.map((skill, index) => (
+          <li key={index}>{skill}</li>
+        ))}
       </ul>
 
-      <h2>Licenses & Certifications</h2>
-      <ul>
-        <li>Learners License (P License anticipated within this year/next year)</li>
-        <li>White Card (Construction Induction Card)</li>
-        <li>EWP Operator License (Elevated Work Platform Operator)</li>
-        <li>Working at Heights certification</li>
-      </ul>
+      <h2>Certifications</h2>
+      {certifications.map((certification, index) => (
+        <div key={index}>
+          <h3>{certification.name}</h3>
+          <p><strong>Date:</strong> {certification.date}</p>
+          <ul>
+            {certification.details.map((detail, idx) => (
+              <li key={idx}>{detail}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
 
       <h2>Experience</h2>
       <ul>
